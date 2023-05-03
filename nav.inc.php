181a889aed4b3871d6c3e2c2231535e90d1c568e
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-05-03 10:33:55 +0200

    feat: navigatie

---
+++ nav.inc.php
@@ -0,0 +1,51 @@
+<div class="nav">
+    <div class="nav_box">
+        <svg width="30" height="34" viewBox="0 0 30 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 16.4145C1 13.7342 1 12.394 1.54182 11.216C2.08363 10.038 3.10117 9.16579 5.13624 7.42144L7.11035 5.72935C10.7887 2.57645 12.6279 1 14.8187 1C17.0095 1 18.8487 2.57645 22.5271 5.72935L24.5012 7.42144C26.5363 9.16579 27.5538 10.038 28.0956 11.216C28.6375 12.394 28.6375 13.7342 28.6375 16.4145V24.7855C28.6375 28.5079 28.6375 30.3691 27.481 31.5255C26.3246 32.6819 24.4634 32.6819 20.741 32.6819H8.89641C5.17401 32.6819 3.31281 32.6819 2.1564 31.5255C1 30.3691 1 28.5079 1 24.7855V16.4145Z" stroke="#C6EBBE" />
+            <path d="M10.8835 21.6554H18.754C19.0294 21.6554 19.254 21.8796 19.254 22.1566V31.9988C19.254 32.2757 19.0294 32.5 18.754 32.5H10.8835C10.6067 32.5 10.3835 32.2765 10.3835 32.0009V22.1527C10.3835 21.8783 10.6056 21.6554 10.8835 21.6554Z" stroke="#C6EBBE" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <a href="index.php">Home</a>
+    </div>
+    <div class="nav_box">
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="1" y="6.33334" width="32" height="26.6667" rx="2" stroke="#C6EBBE" />
+            <path d="M1 15.2222L33 15.2222" stroke="#C6EBBE" stroke-linecap="round" />
+            <path d="M11.6667 24.1111H22.3333" stroke="#C6EBBE" stroke-linecap="round" />
+            <path d="M9.88888 1L9.88888 8.11111" stroke="#C6EBBE" stroke-linecap="round" />
+            <path d="M24.1111 1L24.1111 8.11111" stroke="#C6EBBE" stroke-linecap="round" />
+        </svg>
+        <a href="agenda.php">Agenda</a>
+    </div>
+    <div class="nav_box">
+        <svg width="28" height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M27.1333 26.2632V10.2632C27.1333 7.89779 27.1333 6.71509 26.5042 5.8655C26.2318 5.4977 25.8818 5.18191 25.4741 4.93615C24.5325 4.36847 23.2217 4.36847 20.6 4.36847H7.53333C4.91167 4.36847 3.60084 4.36847 2.6592 4.93615C2.25156 5.18191 1.90156 5.4977 1.62918 5.8655C1 6.71509 1 7.89779 1 10.2632V26.2632C1 29.439 1 31.0269 2.09347 32.0134C3.18694 33 4.94685 33 8.46667 33H19.6667C23.1865 33 24.9464 33 26.0399 32.0134C27.1333 31.0269 27.1333 29.439 27.1333 26.2632Z" stroke="#C6EBBE" />
+            <path d="M8.46666 4.36842C8.46666 2.50809 10.1381 1 12.2 1H15.9333C17.9952 1 19.6667 2.50809 19.6667 4.36842C19.6667 6.22875 17.9952 7.73684 15.9333 7.73684H12.2C10.1381 7.73684 8.46666 6.22875 8.46666 4.36842Z" fill="#0B6253" stroke="#C6EBBE" />
+            <path d="M8.46666 16.1579L19.6667 16.1579" stroke="#C6EBBE" stroke-linecap="round" />
+            <path d="M8.46666 22.8947L15.9333 22.8947" stroke="#C6EBBE" stroke-linecap="round" />
+        </svg>
+        <a href="takenlijst.php">Taken</a>
+    </div>
+    <div class="nav_box">
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="17" cy="17" r="16" stroke="#C6EBBE" />
+            <path d="M17.8889 8.99997C17.8889 9.49089 17.4909 9.88886 17 9.88886C16.5091 9.88886 16.1111 9.49089 16.1111 8.99997C16.1111 8.50905 16.5091 8.11108 17 8.11108C17.4909 8.11108 17.8889 8.50905 17.8889 8.99997Z" fill="#C6EBBE" />
+            <path d="M17 25.8889V13.4444" stroke="#C6EBBE" />
+        </svg>
+        <a href="informatie.php">Info</a>
+    </div>
+</div>
+
+<script>
+    // Get all elements with class "nav_box"
+    const navBoxes = document.querySelectorAll('.nav_box');
+
+    // Loop through each nav box and attach a click event listener
+    navBoxes.forEach(box => {
+        box.addEventListener('click', () => {
+            // Get the link from the <a> tag inside the clicked nav box
+            const link = box.querySelector('a').href;
+            // Navigate to the link
+            window.location.href = link;
+        });
+    });
+</script>

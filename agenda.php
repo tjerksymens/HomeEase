89a2d6a22dfb7c2c7b284afc57c04bb150c39dad
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-05-03 09:37:42 +0200

    startfiles

---
+++ agenda.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"> <!-- Link naar de css file -->
+    <title>HomeEase Agenda</title>
+</head>
+
+<body>
+    <h1>Agenda</h1>
+
+</body>
+
+</html>
---
+++ takenlijst.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"> <!-- Link naar de css file -->
+    <title>HomeEase Takenlijst</title>
+</head>
+
+<body>
+    <h1>Takenlijst</h1>
+</body>
+
+</html>
